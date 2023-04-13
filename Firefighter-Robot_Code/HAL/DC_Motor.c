5da302d7f852aa161ba0af94893aee11bd7a05cb
Abdalla7Ahmed <abdallahahmed17120@gmail.com>
2023-04-14 00:32:17 +0200

    DC motor

---
+++ Firefighter-Robot_Code/HAL/DC_Motor.c
@@ -0,0 +1,79 @@
+/*
+ * DC_Motor.c
+ *
+ *  Created on: ١٣‏/٠٤‏/٢٠٢٣
+ *      Author: Abotaleb
+ */
+
+
+#include "DC_Motor.h"
+
+
+
+TIMx_config_t Timer2;
+
+void HAL_PWM_Set(uint32_t Duty_cycle,uint8_t Motor)
+{
+	Timer2.MODE = TIMx_MODE_PWM1;
+	Timer2.COUNT_MODE = TIMx_COUNT_MODE_UP;
+	Timer2.Prescalers =8;      // 1M
+	Timer2.TopValue = Top_Value;
+	Timer2.CompareValue = Duty_cycle;
+
+	MCAL_TIMx_Init(TIM2, &Timer2, Motor);
+}
+
+
+
+void Car_Routation(char angle , char direction)
+{
+
+	// todo equation to calculate the duty cycle from angle
+
+	if(direction == Car_TurnLeft)
+	{
+		// by iteration set the duty cycle  according to specific angle to the Front_Right_Motor
+		HAL_PWM_Set(_50_Duty_cycle,Front_Right_Motor);
+
+		// stop Front_Left_Motor
+		HAL_PWM_Set(_0_Duty_cycle,Front_Left_Motor);
+		// stop Back_Left_Motor
+		HAL_PWM_Set(_0_Duty_cycle,Back_Left_Motor);
+		// stop Back_Right_Motor
+		HAL_PWM_Set(_0_Duty_cycle,Back_Right_Motor);
+
+	}
+	else if(direction == Car_TurnRight)
+	{
+		// by iteration set the duty cycle  according to specific angle to the Front_Left_Motor
+		HAL_PWM_Set(_50_Duty_cycle,Front_Left_Motor);
+
+		// stop Front_Right_Motor
+		HAL_PWM_Set(_0_Duty_cycle,Front_Right_Motor);
+		// stop Back_Left_Motor
+		HAL_PWM_Set(_0_Duty_cycle,Back_Left_Motor);
+		// stop Back_Right_Motor
+		HAL_PWM_Set(_0_Duty_cycle,Back_Right_Motor);
+	}
+}
+
+
+
+void Car_Move(char speed , char distance)
+{
+	// by iteration set duty cycle to specific speed
+	HAL_PWM_Set(_50_Duty_cycle,Front_Left_Motor);
+	HAL_PWM_Set(_50_Duty_cycle,Front_Right_Motor);
+	HAL_PWM_Set(_50_Duty_cycle,Back_Left_Motor);
+	HAL_PWM_Set(_50_Duty_cycle,Back_Right_Motor);
+
+	// by iteration set delay to specific distance
+	delay_ms(2000);
+
+	// stop the movement
+	HAL_PWM_Set(_0_Duty_cycle,Front_Left_Motor);
+	HAL_PWM_Set(_0_Duty_cycle,Front_Right_Motor);
+	HAL_PWM_Set(_0_Duty_cycle,Back_Left_Motor);
+	HAL_PWM_Set(_0_Duty_cycle,Back_Right_Motor);
+
+}
---
+++ Firefighter-Robot_Code/HAL/inc/DC_Motor.h
@@ -0,0 +1,61 @@
+/*
+ * DC_Motor.h
+ *
+ *  Created on: ١٣‏/٠٤‏/٢٠٢٣
+ *      Author: Abotaleb
+ */
+
+#ifndef INC_DC_MOTOR_H_
+#define INC_DC_MOTOR_H_
+
+
+#include "stm32_f103c6_TIM2_3.h"
+#include "delay.h"
+
+
+#define Front_Left_Motor 								CH1
+#define Front_Right_Motor 								CH2
+#define Back_Left_Motor 								CH3
+#define Back_Right_Motor 								CH4
+
+
+
+//Directions
+#define Car_TurnRight									0
+#define Car_TurnLeft									1
+
+
+#define Top_Value										(uint16_t)10000
+
+#define _Step_5_Duty_Cycle								(uint16_t)(Top_Value / 20)
+
+
+#define _0_Duty_cycle									(uint16_t)(Top_Value - (0*_Step_5_Duty_Cycle))
+#define _5_Duty_cycle									(uint16_t)(Top_Value - (1*_Step_5_Duty_Cycle))
+#define _10_Duty_cycle									(uint16_t)(Top_Value - (2*_Step_5_Duty_Cycle))
+#define _15_Duty_cycle									(uint16_t)(Top_Value - (3*_Step_5_Duty_Cycle))
+#define _20_Duty_cycle									(uint16_t)(Top_Value - (4*_Step_5_Duty_Cycle))
+#define _25_Duty_cycle									(uint16_t)(Top_Value - (5*_Step_5_Duty_Cycle))
+#define _30_Duty_cycle									(uint16_t)(Top_Value - (6*_Step_5_Duty_Cycle))
+#define _35_Duty_cycle									(uint16_t)(Top_Value - (7*_Step_5_Duty_Cycle))
+#define _40_Duty_cycle									(uint16_t)(Top_Value - (8*_Step_5_Duty_Cycle))
+#define _45_Duty_cycle									(uint16_t)(Top_Value - (9*_Step_5_Duty_Cycle))
+#define _50_Duty_cycle									(uint16_t)(Top_Value - (10*_Step_5_Duty_Cycle))
+#define _55_Duty_cycle									(uint16_t)(Top_Value - (11*_Step_5_Duty_Cycle))
+#define _60_Duty_cycle									(uint16_t)(Top_Value - (12*_Step_5_Duty_Cycle))
+#define _65_Duty_cycle									(uint16_t)(Top_Value - (13*_Step_5_Duty_Cycle))
+#define _70_Duty_cycle									(uint16_t)(Top_Value - (14*_Step_5_Duty_Cycle))
+#define _75_Duty_cycle									(uint16_t)(Top_Value - (15*_Step_5_Duty_Cycle))
+#define _80_Duty_cycle									(uint16_t)(Top_Value - (16*_Step_5_Duty_Cycle))
+#define _85_Duty_cycle									(uint16_t)(Top_Value - (17*_Step_5_Duty_Cycle))
+#define _90_Duty_cycle									(uint16_t)(Top_Value - (18*_Step_5_Duty_Cycle))
+#define _95_Duty_cycle									(uint16_t)(Top_Value - (19*_Step_5_Duty_Cycle))
+#define _100_Duty_cycle									(uint16_t)(Top_Value - (20*_Step_5_Duty_Cycle))
+
+
+void HAL_PWM_Set(uint32_t Duty_cycle,uint8_t Motor);
+void Car_Routation(char angle , char direction);
+void Car_Move(char speed , char distance);
+
+
+#endif /* INC_DC_MOTOR_H_ */

6781cb24d0257c9573b1c1ed50299ec6a9c2b05b
BishoyRefaatGaber <refaatbishoy455@gmail.com>
2023-04-13 10:00:37 +0200

    Application

---
+++ Firefighter-Robot_Code/HAL/inc/APIs.h
@@ -0,0 +1,98 @@
+#ifndef APIS_H_
+#define APIS_H_
+
+
+#include <stm32_f103c6_TIM2_3.h>
+#include"stm32_f103c6_GPIO.h"
+#include"stm32_f103c6_EXTI.h"
+#include "stm32f103x6.h"
+#include"stm32_f103c6_USART.h"
+#include"stm32_f103c6_SPI.h"
+#include "stm32_f103c6_I2C.h"
+#include "LCD.h"
+#include "Keypad.h"
+#include "EEPROM.h"
+#include "delay.h"
+#include "Servo.h"
+#include "stm32_f103c6_ADC.h"
+
+
+
+
+//Speeds
+#define Car_Speed_
+#define Car_Speed_
+#define Car_Speed_
+#define Car_Speed_
+#define Car_Speed_
+
+//Directions
+#define Car_TurnRight
+#define Car_TurnLeft
+
+#define Servo_TurnRight
+#define Servo_TurnLeft
+
+//Angles
+#define Car_RoutatioAngle_
+#define Car_RoutatioAngle_
+#define Car_RoutatioAngle_
+#define Car_RoutatioAngle_
+#define Car_RoutatioAngle_
+
+
+#define Servo_RoutatioAngle_
+#define Servo_RoutatioAngle_
+#define Servo_RoutatioAngle_
+#define Servo_RoutatioAngle_
+#define Servo_RoutatioAngle_
+
+
+
+
+int Flame_FrontMid_Read();
+int Flame_FrontRight_Read();
+int Flame_FrontLeft_Read();
+int Flame_Back_Read();
+int Flame_Right_Read();
+int Flame_Left_Read();
+
+
+int Ultrasnic_Read();
+
+void Car_Routation(char angle , char direction);
+void Car_Move(char speed , char distance);
+void Servo_RotationAngle(char angle , char dirction);
+
+
+
+
+
+
+
+
+
+
+
+void Clock_INIT(void)
+{
+    //set on the clock for PORTA
+    RCC_GPIOA_CLK_EN();
+    //set on the clock for PORTB
+    RCC_GPIOB_CLK_EN();
+    //set on the clock for AFIO
+    RCC_GPIOC_CLK_EN();
+    RCC_AFIO_CLK_EN();
+    RCC_TIM2_CLK_Enable();
+    RCC_TIM3_CLK_Enable();
+    RCC_TIM4_CLK_Enable();
+    RCC_ADC1_CLK_Enable();
+}
+
+
+
+
+
+
+
+#endif

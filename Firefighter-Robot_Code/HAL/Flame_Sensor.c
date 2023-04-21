54e6b23617305cc842d10e6765e00f16245273cd
fam ayman <fam.ayman10@gmail.com>
2023-04-21 16:44:06 +0200

    Flame

---
+++ Firefighter-Robot_Code/HAL/Flame_Sensor.c
@@ -0,0 +1,25 @@
+#include "Flame_Sensor.h"
+int Flame_Frontmid_Read()
+{
+	return MCAL_ADC_Read(0);
+}
+int Flame_FrontRight_Read()
+{
+	return MCAL_ADC_Read(1);
+}
+int Flame_FrontLeft_Read()
+{
+	return MCAL_ADC_Read(2);
+}
+int Flame_Back_Read()
+{
+	return MCAL_ADC_Read(3);
+}
+int Flame_Right_Read()
+{
+	return MCAL_ADC_Read(4);
+}
+int Flame_Left_Read()
+{
+	return MCAL_ADC_Read(5);
+}
---
+++ Firefighter-Robot_Code/HAL/inc/Flame_Sensor.h
@@ -0,0 +1,24 @@
+#ifndef Flame_Sensor
+#define Flame_Sensor
+
+
+
+#define Flame_Fromtmid_ch
+#define Flame_FrontRight_ch
+#define Flame_FrontLeft_ch
+#define Flame_Back_ch
+#define Flame_Right_ch
+#define Flame_Left_ch
+
+=================================APIS==================
+
+int Flame_Frontmid_Read();
+int Flame_FrontRight_Read();
+int Flame_FrontLeft_Read();
+int Flame_Back_Read();
+int Flame_Right_Read();
+int Flame_Left_Read();
+
+
+
+#endif

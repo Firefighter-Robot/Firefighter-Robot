e5b1422d915c86ce3892a8cfd1305467ebbcd8de
BishoyRefaatGaber <refaatbishoy455@gmail.com>
2023-04-21 11:50:07 +0200

    Application

--- Firefighter-Robot_Code/HAL/inc/APIs.h
+++ Firefighter-Robot_Code/HAL/inc/APIs.h
@@ -15,17 +15,42 @@
 #include "delay.h"
 #include "Servo.h"
 #include "stm32_f103c6_ADC.h"
+#include "DC_Motor.h"
+#include "Ultrasonic.h"
+
+
+
+
+
 
 
 #define distance_step
 #define angle_step
 
 //Speeds
-#define Car_Speed_
-#define Car_Speed_
-#define Car_Speed_
-#define Car_Speed_
-#define Car_Speed_
+     
+#define Car_Speed_0       _0_Duty_cycle       
+#define Car_Speed_5       _5_Duty_cycle       
+#define Car_Speed_10      _10_Duty_cycle  
+#define Car_Speed_15      _15_Duty_cycle  
+#define Car_Speed_20      _20_Duty_cycle  
+#define Car_Speed_25      _25_Duty_cycle  
+#define Car_Speed_30      _30_Duty_cycle  
+#define Car_Speed_35      _35_Duty_cycle  
+#define Car_Speed_40      _40_Duty_cycle  
+#define Car_Speed_45      _45_Duty_cycle  
+#define Car_Speed_50      _50_Duty_cycle  
+#define Car_Speed_55      _55_Duty_cycle  
+#define Car_Speed_60      _60_Duty_cycle  
+#define Car_Speed_65      _65_Duty_cycle  
+#define Car_Speed_70      _70_Duty_cycle  
+#define Car_Speed_75      _75_Duty_cycle  
+#define Car_Speed_80      _80_Duty_cycle  
+#define Car_Speed_85      _85_Duty_cycle  
+#define Car_Speed_90      _90_Duty_cycle  
+#define Car_Speed_95      _95_Duty_cycle  
+#define Car_Speed_100     _100_Duty_cycle     
+
 
 //Directions
 #define Car_TurnRight
